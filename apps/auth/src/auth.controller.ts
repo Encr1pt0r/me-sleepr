2a52148b13d66cd50d7782c87938eadd4fdff168
Encr1pt0r <presidor16@gmail.com>
2024-05-03 17:10:43 +0100

    dockerise auth

--- apps/auth/src/auth.controller.ts
+++ apps/auth/src/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
-@Controller()
+@Controller('users')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
